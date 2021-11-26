6ea9f646db2da35f7cbf9178fdddd15192ec41c4
Ben Dowsett <bendowsett@gmail.com>
2021-11-26 11:36:19 +0000

    Max temp triggered within CLI

--- namePrompt.js
+++ namePrompt.js
@@ -1,23 +1,24 @@
-var readlineSync = require('readline-sync');
-const Thermostat = require('./thermostat');
-readlineSync.setDefaultOptions({limit: ['up', 'down', 'psm on', 'psm off']});
+var readlineSync = require("readline-sync");
+const Thermostat = require("./thermostat");
+readlineSync.setDefaultOptions({ limit: ["up", "down", "psm on", "psm off"] });
 
-therm = new Thermostat
-console.log(therm.getTemperature()) 
+therm = new Thermostat();
+console.log(therm.getTemperature());
 
-while (true) {  
+while (true) {
+  var input = readlineSync.question("Enter Command > ");
+  //if (therm.temp === therm.maxtemp){
+  //console.log(`${therm.getTemperature()} (Maximum reached)`)
+  // }
 
-var input = readlineSync.question("Enter Command > ");
-if (input === 'up') {
-    therm.up(1)
-    console.log(therm.getTemperature())
-} else if 
-    (input === 'down') {
-    therm.down(1)
-    console.log(therm.getTemperature())
-    }
+  if (input === "up") {
+    console.log(therm.up(1));
+  } else if (input === "down") {
+    therm.down(1);
+    console.log(therm.getTemperature());
+  } else if (input === "psm on") {
+    therm.setPowerSavingMode(true);
+  } else if (input === "psm off") {
+    therm.setPowerSavingMode(false);
+  }
 }
-
-
-
-
--- thermostat.js
+++ thermostat.js
@@ -1,55 +1,50 @@
 class Thermostat {
-    constructor(){
-        this.temp = 20
-        this.powersaver = true
-        this.maxtemp = 25
-        
+  constructor() {
+    this.temp = 20;
+    this.powersaver = true;
+    this.maxtemp = 25;
+  }
+  getTemperature() {
+    return `Temperature is ${this.temp}`;
+  }
 
+  up(number) {
+    if (this.temp + number > this.maxtemp) {
+      //this.temp = this.temp
+      return `${this.getTemperature()} (max reached)`;
+    } else {
+      return `Temperature is ${(this.temp += number)}`;
     }
-getTemperature() {
-   return `Temperature is ${this.temp}`
-}    
+  }
 
-up(number){
-   if (this.temp + number > this.maxtemp)
-   { this.temp = this.temp
-    return "max reached"}
-   else
-   {return this.temp += number}
-}
-
-down(number){
-    if (this.temp - number < 10 ) {
-        this.temp = this.temp
-        return "min temp is 10 degrees"
-    }else{
-        return this.temp -= number}
-}
-
-setPowerSavingMode(boolean){
-    if (boolean == false){
-        this.maxtemp = 32
-    }else if (boolean == true){
-        this.maxtemp = 25
+  down(number) {
+    if (this.temp - number < 10) {
+      this.temp = this.temp;
+      return "min temp is 10 degrees";
+    } else {
+      return (this.temp -= number);
     }
+  }
 
+  setPowerSavingMode(boolean) {
+    if (boolean == false) {
+      this.maxtemp = 32;
+    } else if (boolean == true) {
+      this.maxtemp = 25;
+    }
+  }
+  reset() {
+    return (this.temp = 20);
+  }
+  currentEnergyUsage() {
+    if (this.temp < 18) {
+      return "low usage";
+    } else if (this.temp <= 25) {
+      return "medium-usage";
+    } else {
+      return "high usage";
+    }
+  }
 }
-
-reset(){
-    return this.temp = 20
-
-}
-
-currentEnergyUsage(){
-   if (this.temp < 18) {
-       return "low usage"
-   }else if (this.temp <= 25){
-       return "medium-usage"
-   }else {return "high usage"}
-
-}
-
-}
-
 
 module.exports = Thermostat;
