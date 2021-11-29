4438174f3dee6167f7def7dee75bed8471a2da10
Ben Dowsett <85438016+bdowsett@users.noreply.github.com>
2021-11-29 14:20:07 +0000

    Update weather.js

--- apis/weather/weather.js
+++ apis/weather/weather.js
@@ -5,7 +5,7 @@ class Weather {
 
 fetchWeatherData(city, callback){
   const got = require('got');
-  const apiKey = '6b5b16ea8cf3f646723de0de183641a1'; // paste your API key here
+  const apiKey = 
   const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
   city = city;
 
